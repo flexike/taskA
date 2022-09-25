3d072db37d29362f2c5392c28b389962f7066284
flexike <flexike@ukr.net>
2022-09-26 02:46:03 +0300

    change filtering work; delete non-use files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import {useState, useEffect} from "react";
-import './style/style.css';
 import CardPeople from './components/card';
-import {database} from "./http/database";
-import {getData} from "./components/filtering";
-import Filter from "./components/filtering";
+import {getData} from "../http";
+import {Button, Form, Select} from "antd";
+import './style/style.css';
+const { Option } = Select;
 
 
 function App() {
 
     const [db, setDb] = useState()
+    const [gender, setGender] = useState('all')
+    const [nationality, setNationality] = useState('all')
+    const [filt, setFilt] = useState([])
 
     useEffect(() => {
-            database().then(({data}) => {
-                setDb(data)
-        })
-        //   getData().then(r => setDb(r)) - gender\nat ?
+
     }, [])
 
     if (!db) return <div>Loading...</div>
@@ -36,9 +36,74 @@ function App() {
         )
     })
 
+    const layout = {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 16 },
+    };
+
+    function onNationalityChange(e) {
+        setNationality(e)
+    }
+
+    function onGenderChange(e) {
+        setGender(e)
+    }
+
   return (
     <div className="asas">
-        <Filter/>
+        <Form {...layout} className="formM" name="form">
+            <Form.Item
+                name="gender"
+            >
+                <Select
+                    defaultValue="Gender"
+                    style={{ width: 120, marginRight: 25 }}
+                    onChange={onGenderChange}
+                >
+                    <Option value="all">All</Option>
+                    <Option value="male">Male</Option>
+                    <Option value="female">Female</Option>
+                </Select>
+            </Form.Item>
+
+            <Form.Item
+                name="nationality"
+            >
+                <Select
+                    defaultValue="Nationality"
+                    style={{ width: 120 }}
+                    onChange={onNationalityChange}
+                >
+                    <Option value="all">All</Option>
+                    <Option value="au">AU</Option>
+                    <Option value="br">BR</Option>
+                    <Option value="ca">CA</Option>
+                    <Option value="ch">CH</Option>
+                    <Option value="de">DE</Option>
+                    <Option value="dk">DK</Option>
+                    <Option value="es">ES</Option>
+                    <Option value="fi">FI</Option>
+                    <Option value="fr">FR</Option>
+                    <Option value="gb">GB</Option>
+                    <Option value="ie">IE</Option>
+                    <Option value="ir">IR</Option>
+                    <Option value="no">NO</Option>
+                    <Option value="nl">NL</Option>
+                    <Option value="nz">NZ</Option>
+                    <Option value="tr">TR</Option>
+                    <Option value="us">US</Option>
+                </Select>
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button
+                    type="primary"
+                    htmlType="submit"
+                    onClick={() => getData(gender, nationality).then(r => console.log(r))}
+                >
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
         {cards}
     </div>
   );
--- src/http/index.js
+++ src/http/index.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-export async function database() {
-    return axios.get('https://randomuser.me/api/?results=15')
-}
-
 export async function getData(gen, nat) {
     const res = await axios.get(`https://randomuser.me/api/?results=15&gender=${gen}&nat=${nat}`)
     return res.data.results
